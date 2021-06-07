73d7e1d9e576eedae3dd22a288c12a8933fd1df9
Leonor <leonorpatriciasf@hotmail.com>
2021-06-07 19:47:25 +0100

    How many enemies? YES

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,31 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declarar variáveis
+            int numEnemies;
+            Enemy[] enemies; 
+
+            //Quantos inimigos o utilizador quer
+            Console.WriteLine("How many enemies?");
+            numEnemies = Int32.Parse(Console.ReadLine());
+
+            //Cada inimigo, cada nome dado
+            //Criar objecto inimigoe guardá-lo num array
+            enemies = new Enemy[numEnemies];
+
+            for (int i = 0; i < numEnemies; i++)
+            {
+                string name;
+                Console.Write("Escreva un nome: ");
+                name = Console.ReadLine();
+                enemies[i] = new Enemy(name);
+            }
+
+            //Para cada inimigo, impimir seu nome
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine(enemy.GetName());
+            }
         }
     }
 }
